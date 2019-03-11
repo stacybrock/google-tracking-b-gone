1fb487fdbc24c5c61b95d7415d5458f61de8ad59
Stacy Brock <brock@oregonstate.edu>
2019-03-11 10:08:46 -0700

    Neutralize all click events on links

--- google-tracking-b-gone.user.js
+++ google-tracking-b-gone.user.js
@@ -1,26 +1,26 @@
 // Google Tracking-B-Gone
-// version 2.4
-// Release Date: 2017-04-19
+// version 3.0
+// Release Date: 2019-03-11
 // https://greasyfork.org/en/scripts/1810-google-tracking-b-gone
 // https://github.com/stacybrock/google-tracking-b-gone
 //
 // ===== INSTRUCTIONS =====
 //
-// This is a Greasemonkey user script.
+// This is a Tampermonkey user script.
 //
-// To use this script, get Greasemonkey: http://www.greasespot.net
+// To use this script, get Tampermonkey: https://tampermonkey.net
 // After you've installed it, come back to this page. A dialog box will
 // appear asking you if you want to install this script.
 //
-// To uninstall, go to Tools->Greasemonkey->Manage User Scripts, select
-// "Google Tracking-B-Gone" from the list on the left, and click
-// Uninstall.
+// To uninstall, open the Tampermonkey dashboard and click the delete
+// icon next to "Google Tracking-B-Gone" in the list of installed
+// scripts.
 //
 //
 // ==UserScript==
 // @name           Google Tracking-B-Gone
 // @namespace      http://notoriety.org
-// @version        2.4
+// @version        3.0
 // @description    Strips click tracking from Google search results
 //
 // @include        http://www.google.*
@@ -31,92 +31,37 @@
 
 var debug = false;
 
-if (debug) { console.log("Google Tracking-B-Gone initialized."); }
+debug && console.log("Google Tracking-B-Gone initialized.");
+
+var checkMutation = function(mutation) {
+  var target = mutation.target;
+  if (target && target.nodeName.toLowerCase() === 'a') {
+    cleanLink(target);
+  } else if (target instanceof Element) {
+    target.querySelectorAll('a').forEach(cleanLink);
+  }
+};
 
 var changeObserver = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
-    if ((mutation.target.nodeName == 'BODY' && mutation.target.attributes.getNamedItem('id').value == 'gsr') ||
-        (mutation.target.nodeName == 'DIV' && mutation.target.attributes.getNamedItem('id').value == 'taw')) {
-      doIt();
-    }
-  });
+  if (mutations.target) {
+    checkMutation(mutations);
+  } else {
+    mutations.forEach && mutations.forEach(checkMutation);
+  }
 });
-changeObserver.observe(document.documentElement, { childList: true, subtree: true });
+changeObserver.observe(document.documentElement, {
+    childList: true,
+    subtree: true,
+    attributes: true,
+    attributeFilter: ['href']
+});
 
+var blockEvent = function (event) {
+  event.stopPropagation();
+};
 
-function doIt() {
-  if (debug) { console.log("doIt() called..."); }
-  var resultLinks = $x("//a[@onmousedown]", XPathResult.ORDERED_NODE_SNAPSHOT_TYPE);
-  resultLinks.forEach(function(link) {  // loop over links
-    if (link.getAttribute('onmousedown')) {
-      link.removeAttribute('onmousedown');
-    }
-  });
-
-  resultLinks = $x("//a");
-  resultLinks.forEach(function(link) {  // loop over links
-    var oldLink = link.href;
-    if (/^http:\/\/www.google.co/.test(oldLink) || /^https:\/\/encrypted.google.co/.test(oldLink)) {
-      var matches = /url\?(url|q)=(.+?)&/.exec(oldLink);
-      if (matches != null) {
-        link.href = unescape(matches[2]);
-      }
-    } else if ((/pdf$/i).test(oldLink)) {
-      link.href = oldLink;
-    }
-  });
-}
-
-
-// XPath helper, from
-// https://wiki.greasespot.net/XPath_Helper
-function $x() {
-  var x='';
-  var node=document;
-  var type=0;
-  var fix=true;
-  var i=0;
-  var cur;
-
-  function toArray(xp) {
-    var final=[], next;
-    while (next=xp.iterateNext()) {
-      final.push(next);
-    }
-    return final;
-  }
-
-  while (cur=arguments[i++]) {
-    switch (typeof cur) {
-      case "string": x+=(x=='') ? cur : " | " + cur; continue;
-      case "number": type=cur; continue;
-      case "object": node=cur; continue;
-      case "boolean": fix=cur; continue;
-    }
-  }
-
-  if (fix) {
-    if (type==6) type=4;
-    if (type==7) type=5;
-  }
-
-  // selection mistake helper
-  if (!/^\//.test(x)) x="//"+x;
-
-  // context mistake helper
-  if (node!=document && !/^\./.test(x)) x="."+x;
-
-  var result=document.evaluate(x, node, null, type, null);
-  if (fix) {
-    // automatically return special type
-    switch (type) {
-      case 1: return result.numberValue;
-      case 2: return result.stringValue;
-      case 3: return result.booleanValue;
-      case 8:
-      case 9: return result.singleNodeValue;
-    }
-  }
-
-  return fix ? toArray(result) : result;
+function cleanLink(link) {
+  debug && console.log("cleanLink() called...");
+  link.onmousedown = link.onmouseup = link.onmouseenter = link.onmousemove =
+      link.ondbclick = link.onclick = link.oncontextmenu = blockEvent;
 }
